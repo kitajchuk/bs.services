d28ede74199f136d69491d2d4c78c7fa6daaa89f
Kendall Strautman <kendallstraut@gmail.com>
2020-07-01 08:38:14 -0700

    Revert "chore: remove google analytics"
    This reverts commit 9ceb2bc79cbb6bdbaaf430c90c95f58de4eb6c74.

--- source/js/properjs/app.js
+++ source/js/properjs/app.js
@@ -1,9 +1,12 @@
-require("../../sass/screen.scss");
+require( "../../sass/screen.scss" );
+
 
 import router from "./router";
 import * as core from "./core";
 import intro from "./intro";
+import Analytics from "./class/Analytics";
 import $ from "properjs-hobo";
+
 
 /**
  *
@@ -13,54 +16,62 @@ import $ from "properjs-hobo";
  *
  */
 class App {
-  constructor() {
-    this.core = core;
-    this.intro = intro;
-    this.router = router;
+    constructor () {
+        this.core = core;
+        this.intro = intro;
+        this.router = router;
 
-    this.bind();
-    this.init();
-    this.info();
-  }
+        this.bind();
+        this.init();
+        this.info();
+    }
 
-  info() {
-    core.dom.main.on("click", ".js-toggler", (e) => {
-      const elem = $(e.target);
-      const target = $(`.${elem.data().target}`);
 
-      elem.toggleClass("is-active");
-      target.toggleClass("is-active");
-    });
-  }
+    info () {
+        core.dom.main.on( "click", ".js-toggler", ( e ) => {
+            const elem = $( e.target );
+            const target = $( `.${elem.data().target}` );
 
-  bind() {
-    this.core.emitter.on("app--intro-teardown", () => {
-      this.core.log("App Intro Teardown");
-    });
+            elem.toggleClass( "is-active" );
+            target.toggleClass( "is-active" );
+        });
+    }
 
-    this.core.emitter.on("app--page-teardown", () => {
-      this.core.log("App Page Teardown");
-    });
-  }
 
-  init() {
-    // Core
-    this.core.detect.init();
+    bind () {
+        this.core.emitter.on( "app--intro-teardown", () => {
+            this.core.log( "App Intro Teardown" );
+        });
 
-    // Views
-    this.intro.init();
+        this.core.emitter.on( "app--page-teardown", () => {
+            this.core.log( "App Page Teardown" );
+        });
+    }
 
-    // Controller
-    this.router.init();
-  }
+
+    init () {
+        // Core
+        this.core.detect.init();
+
+        // Views
+        this.intro.init();
+
+        // Controller
+        this.router.init();
+
+        // Analytics
+        this.analytics = new Analytics();
+    }
 }
+
 
 /******************************************************************************
  * Expose
- *******************************************************************************/
+*******************************************************************************/
 window.app = new App();
+
 
 /******************************************************************************
  * Export
- *******************************************************************************/
+*******************************************************************************/
 export default window.app;
