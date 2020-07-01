9ceb2bc79cbb6bdbaaf430c90c95f58de4eb6c74
Kendall Strautman <kendallstraut@gmail.com>
2020-07-01 08:28:57 -0700

    chore: remove google analytics

--- source/js/properjs/app.js
+++ source/js/properjs/app.js
@@ -1,12 +1,9 @@
-require( "../../sass/screen.scss" );
-
+require("../../sass/screen.scss");
 
 import router from "./router";
 import * as core from "./core";
 import intro from "./intro";
-import Analytics from "./class/Analytics";
 import $ from "properjs-hobo";
-
 
 /**
  *
@@ -16,62 +13,54 @@ import $ from "properjs-hobo";
  *
  */
 class App {
-    constructor () {
-        this.core = core;
-        this.intro = intro;
-        this.router = router;
+  constructor() {
+    this.core = core;
+    this.intro = intro;
+    this.router = router;
 
-        this.bind();
-        this.init();
-        this.info();
-    }
+    this.bind();
+    this.init();
+    this.info();
+  }
 
+  info() {
+    core.dom.main.on("click", ".js-toggler", (e) => {
+      const elem = $(e.target);
+      const target = $(`.${elem.data().target}`);
 
-    info () {
-        core.dom.main.on( "click", ".js-toggler", ( e ) => {
-            const elem = $( e.target );
-            const target = $( `.${elem.data().target}` );
+      elem.toggleClass("is-active");
+      target.toggleClass("is-active");
+    });
+  }
 
-            elem.toggleClass( "is-active" );
-            target.toggleClass( "is-active" );
-        });
-    }
+  bind() {
+    this.core.emitter.on("app--intro-teardown", () => {
+      this.core.log("App Intro Teardown");
+    });
 
+    this.core.emitter.on("app--page-teardown", () => {
+      this.core.log("App Page Teardown");
+    });
+  }
 
-    bind () {
-        this.core.emitter.on( "app--intro-teardown", () => {
-            this.core.log( "App Intro Teardown" );
-        });
+  init() {
+    // Core
+    this.core.detect.init();
 
-        this.core.emitter.on( "app--page-teardown", () => {
-            this.core.log( "App Page Teardown" );
-        });
-    }
+    // Views
+    this.intro.init();
 
-
-    init () {
-        // Core
-        this.core.detect.init();
-
-        // Views
-        this.intro.init();
-
-        // Controller
-        this.router.init();
-
-        // Analytics
-        this.analytics = new Analytics();
-    }
+    // Controller
+    this.router.init();
+  }
 }
-
 
 /******************************************************************************
  * Expose
-*******************************************************************************/
+ *******************************************************************************/
 window.app = new App();
-
 
 /******************************************************************************
  * Export
-*******************************************************************************/
+ *******************************************************************************/
 export default window.app;
